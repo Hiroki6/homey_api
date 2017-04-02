45b53d9220f9470837b93bebe5325b9df93faed9
fuzyco <bkn4xuimp.gvr2kg.e@gmail.com>
2017-04-02 18:02:09 +0900

    add dialot method

--- dialog.py
+++ dialog.py
@@ -3,6 +3,7 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 import numpy as np
 from PNJudge import judge
+from rnn import exec_attention
 
 app = Flask(__name__)
 
@@ -23,6 +24,15 @@ def pn_judge(sentense):
     value = judge.judge_pn(sentense)
     result = "ポジティブ" if value > 0 else "ネガティブ"
     return result
+
+@app.route('/dialog/', methods=['POST'])
+def dialog():
+    user_message = request.form['user_message']
+    response = exec_attention.get_response(user_message, "rnn/attention-models/attention-24.model")
+    result = {
+            "response": response
+            }
+    return jsonify(ResultSet=result)
 
 if __name__ == "__main__":
     app.run()
