d0ccce432cbd62fde226372000759fa8710a7077
fuzyco <bkn4xuimp.gvr2kg.e@gmail.com>
2017-04-02 19:18:59 +0900

    bugfix

--- dialog.py
+++ dialog.py
@@ -26,7 +26,7 @@ def pn_judge(sentense):
     return result
 
 @app.route('/dialog/<user_message>', methods=['GET'])
-def dialog():
+def dialog(user_message):
     print user_message
     response = exec_attention.get_response(user_message, "rnn/attention-models/attention-24.model")
     result = {
